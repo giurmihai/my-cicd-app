33c11666ac4f99c4e7532e954aea60932497c230
Mihai Giurgiteanu <giurgiteanu.ma@gmail.com>
2025-11-07 18:05:02 +0200

    Testing CICD updating

--- src/MainPage.js
+++ src/MainPage.js
@@ -5,7 +5,8 @@ function MainPage() {
   return (
     <div className="MainPage">
       This page here is only for having an app to test automation on. Not an actual website.<br></br>
-      Made by: Mihai Giurgiteanu
+      Made by: Mihai Giurgiteanu<br></br>
+      Update: This is an update to test the CICD
     </div>
   );
 }
