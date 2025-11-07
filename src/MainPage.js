ef91341d533f81ee10394130b3fda70fcd9ce4bb
Mihai Giurgiteanu <giurgiteanu.ma@gmail.com>
2025-11-07 17:48:04 +0200

    Initial commit

---
+++ src/MainPage.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MainPage() {
+  return (
+    <div className="MainPage">
+      This page here is only for having an app to test automation on. Not an actual website.<br></br>
+      Made by: Mihai Giurgiteanu
+    </div>
+  );
+}
+
+export default MainPage;
